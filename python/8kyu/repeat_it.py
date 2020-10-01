51e66e21a917adae275bedae6c6e45392551c307
ckula-CPT <ckula@student.wethinkcode.co.za>
2020-10-01 16:34:12 +0200

    Daily excercises

---
+++ python/8kyu/repeat_it.py
@@ -0,0 +1,21 @@
+"""
+Create a function that takes a string and an integer (n).
+
+The function should return a string that repeats the input string n number of times.
+
+If anything other than a string is passed in you should return "Not a string"
+"""
+
+def repeat_it(string, n):
+    if string == "" or string == []:
+        return "Not a string"
+    return str(string) * n
+    
+
+
+
+if __name__ == "__main__":
+    print("No string: " + str(repeat_it("", 2)))
+    print("Empty list: " +str(repeat_it([], 1)))
+    print("Repeat name: " + str(repeat_it("Odwa", 3)))
+
